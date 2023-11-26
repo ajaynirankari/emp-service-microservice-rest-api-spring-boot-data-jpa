52007668b3a3739b4706072a343ce86e5343995a
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2023-11-26 11:06:39 +0530

    add the post and delete endpoints

--- src/main/java/com/gl/epmservice/EpmServiceApplication.java
+++ src/main/java/com/gl/epmservice/EpmServiceApplication.java
@@ -13,8 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -46,6 +49,16 @@ class EmpController {
     @GetMapping("/emps/{id}")
     public Optional<Emp> one(@PathVariable long id) {
         return repo.findById(id);
+    }
+
+    @PostMapping("/emps")
+    public Emp create(@RequestBody Emp emp) {
+        return repo.save(emp);
+    }
+
+    @DeleteMapping("/emps/{id}")
+    public void delete(@PathVariable long id) {
+        repo.deleteById(id);
     }
 }
 
